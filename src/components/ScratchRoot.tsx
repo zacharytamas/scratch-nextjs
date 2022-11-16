982d8c91d17011888a91e2f86fa00e673d8dec9e
Zachary Jones <zachary@jones.cx>
2022-11-16 13:27:05 -0500

    chore: Upgrade dependencies to latest

--- src/components/ScratchRoot.tsx
+++ src/components/ScratchRoot.tsx
@@ -1,16 +1,14 @@
 import Head from 'next/head';
-import React, { Component } from 'react';
+import Script from 'next/script';
 
-export class ScratchRoot extends Component {
-  render() {
-    return (
-      <>
-        <Head>
-          <title>CSA Sim</title>
-          <script src="//cdn.polyfill.io/v2/polyfill.min.js" />
-        </Head>
-        {this.props.children}
-      </>
-    );
-  }
-}
+export const ScratchRoot = ({ children }) => {
+  return (
+    <>
+      <Head>
+        <title>Scratch Next.js</title>
+        <Script src="//cdn.polyfill.io/v2/polyfill.min.js" />
+      </Head>
+      {children}
+    </>
+  );
+};
